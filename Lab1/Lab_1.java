0b4dc5b1dc1475179f9842c45e35dd304b20466e
cthacker-udel <70614147+cthacker-udel@users.noreply.github.com>
2020-09-03 13:35:00 -0400

    Update Lab_1.java

--- Lab1/Lab_1.java
+++ Lab1/Lab_1.java
@@ -2,5 +2,8 @@ import java.util.*;
 public class Lab_1 {
 	public static void main(String[] args) {
 		Scanner input = new Scanner(System.in);
+		
+		System.out.println("Please enter your income amount");
+		double income = input.nextDouble();
 	}
 }
