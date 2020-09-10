290cfd690aebc04cc415962dbc20669bf8946408
wesorth <70714434+wesorth@users.noreply.github.com>
2020-09-10 12:39:03 -0400

    Add files via upload

---
+++ TaxLab1/GIT/Helloworld/src/HelloWorld.java
@@ -0,0 +1,14 @@
+
+public class HelloWorld {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		int x = 1;
+        int y = x = x + 1;
+        System.out.println("y is " + y);
+
+
+
+	}
+
+}
